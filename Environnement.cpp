b837e8d4bb8c8a64974e7e11ecb4075995c4825c
Justine Banc <justineh04@gmail.com>
2022-11-27 15:51:36 +0100

    Constructeur de la classe Environnement

--- Environnement.cpp
+++ Environnement.cpp
@@ -1,39 +1,47 @@
-#include "Environement.hpp"
-#define G  6,6742e-11;
+#include "Environnement.hpp"
+
+//#define G  6.6742e-11;
 using namespace std;
 
-bool Environement::touche_ou_pas()
-{
-  if(bird.get_poste_fixe() + bird.get_rayon() < obstacle_arriere.get_poste_fixe() - obstacle_arriere.get_large() ||
-     bird.get_poste_fixe() - bird.get_rayon() > obstacle_arriere.get_poste_fixe() + obstacle_arriere.get_large() && bird.get_poste_fixe() + bird.get_rayon() < obstacle_avant.get_poste_fixe() - obstacle_avant.get_large() ||
-     bird.get_poste_fixe() - bird.get_rayon() > obstacle_avant.get_poste_fixe())
-    return(false);
-  else if( (bird.get_pos() - bird.get_rayon() > obstacle_arriere.get_bas() && bird.get_pos() + bird.get_rayon() < obstacle_arriere.get_haut()) ||
-           (bird.get_pos() - bird.get_rayon() > obstacle_arriere.get_bas() && bird.get_pos() + bird.get_rayon() < obstacle_arriere.get_haut())
-    return(false);
-  else
-    return(true);
+template<class RNG>
+bool Environnement<RNG>::touche_ou_pas(void) {
+    if (bird.get_pos_hor() + bird.get_rayon() < obstacle_arriere.get_pos_hor() - obstacle_arriere.get_large() ||
+        bird.get_pos_hor() - bird.get_rayon() > obstacle_arriere.get_pos_hor() + obstacle_arriere.get_large() &&
+        bird.get_pos_hor() + bird.get_rayon() < obstacle_avant.get_pos_hor() - obstacle_avant.get_large() ||
+        bird.get_pos_hor() - bird.get_rayon() > obstacle_avant.get_pos_hor())
+        return (false);
+    else if ((bird.get_pos() - bird.get_rayon() > obstacle_arriere.get_bas() &&
+              bird.get_pos() + bird.get_rayon() < obstacle_arriere.get_haut()) ||
+             (bird.get_pos() - bird.get_rayon() > obstacle_arriere.get_bas() &&
+              bird.get_pos() + bird.get_rayon() < obstacle_arriere.get_haut()))
+        return (false);
+    else if (bird.get_pos() < 0 || bird.get_pos() > taille_fenetre_verticale)
+        return (false);
+    else
+        return (true);
 }
 
-void Environement::change_obstacle_si_necessaire()
-{
-  if( obstacle_arriere.get_pos() + obstacle_arriere.get_large() <= 0 ) // coordonées de la fenetre (0,0) en bas a gauche !
-  {
-    obstacle_arriere = obstacle_avant;
-    obstacle_avant = Obstacle(aleas, taille_fenetre_horizontale, taille_fenetre_verticale, bird.get_rayon());
-  }
+template<class RNG>
+void Environnement<RNG>::change_obstacle_si_necessaire() {
+    if (obstacle_arriere.get_pos_hor() + obstacle_arriere.get_large() <=
+        0) // coordonées de la fenetre (0,0) en bas a gauche !
+    {
+        obstacle_arriere = obstacle_avant;
+        obstacle_avant = Obstacle(aleas, taille_fenetre_horizontale, taille_fenetre_verticale, bird.get_rayon());
+    }
 }
 
-void Environnement::avance_oiseau()
-{
-  double poid = bird.get_masse()*G;
-  // double Force_ini = 543;
-  double a = (poid + Force_ini)/bird.get_masse();
-  bird.set_vit(bird.get_vit() + dt * a);
-  bird.set_pos(bird.get_pos() + dt * bird.get_vit() + dt * dt/2 * a)
+template<class RNG>
+void Environnement<RNG>::avance_oiseau() {
+    cout<<"casse les couilles"<<endl;
+    double poids = bird.get_masse() * Gr;
+    double a = (poids) / bird.get_masse();
+    bird.set_vit(bird.get_vit() + dt * a);
+    bird.set_pos(bird.get_pos() + dt * bird.get_vit() + dt * dt / 2 * a);
 }
 
-void Environnement::avance_tuyaux()
-{
-  obstacle_avant.set_pos(obstacle_avant.get_pos() + dt*vitesse_orizontale + vitesse_orizontale) // /!\ valable que si la vitessse est constante !!
+template<class RNG>
+void Environnement<RNG>::avance_tuyaux() {
+//TODO A Finir !!
+    obstacle_avant.set_pos_hor(obstacle_avant.get_pos_hor() + dt * vitesse_horizontale); // /!\ valable que si la vitessse est constante !!
 }
--- Environnement.hpp
+++ Environnement.hpp
@@ -5,34 +5,45 @@
 #include "Obstacle.h"
 #include <vector>
 
+
 template<class RNG>
 class Environnement {
-private:
-    double vitesse_orizontale = -50;
+public:
+    double vitesse_horizontale;
 
     Obstacle obstacle_avant;
     Obstacle obstacle_arriere;
 
     Oiseau bird;
 
-    int taille_fenetre_horizontale = 600;
-    int taille_fenetre_verticale = 700;
+    int taille_fenetre_horizontale;
+    int taille_fenetre_verticale;
 
     RNG aleas;
 
-    int dt = 1. / 60;// 1/fps
+    double dt;
 
-public:
-    //Environnement() : aleas(RNG){};
-    bool touche_ou_pas();
+    double Gr;
+
+// public:
+    Environnement(RNG G, double VH = -50., int T_horiz = 800, int T_vert = 1080, int ray_obs = 40,
+                  double dt_ = 1. / 60, double Gr_ = 6.6742e-11) : vitesse_horizontale(VH),
+                                                                   obstacle_avant(G, T_horiz / 2, T_vert, ray_obs),
+                                                                   obstacle_arriere(G, T_horiz, T_vert, ray_obs),
+                                                                   dt(dt_),
+                                                                   bird(T_horiz / 4, 0., 0.02),
+                                                                   taille_fenetre_horizontale(T_horiz),
+                                                                   taille_fenetre_verticale(T_vert),
+                                                                   Gr(Gr_), aleas(G) {};
+//TODO Pourquoi on se sert de la masse ??
+
+    bool touche_ou_pas(void);
 
     void change_obstacle_si_necessaire();
 
-    // ------A FAIRE-------
     void avance_oiseau();
 
     void avance_tuyaux();
-    //-----------------
 };
 
 #endif
